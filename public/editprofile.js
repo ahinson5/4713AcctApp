b5752206187e2536274820f4b7d0d15016c265e4
Flakey <flakeybisquik@hotmail.com>
2023-04-17 12:52:07 -0400

    Uploading a profile pic
    button under profile pic. can upload a file to change your profile pic.

--- public/editprofile.js
+++ public/editprofile.js
@@ -1,5 +1,36 @@
 import { ShowLoggedInUserInfo } from "./MyUtil";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const storage = getStorage();
+const storageRef = ref(storage, 'profilePic');
+var uploadPicInput = document.querySelector("#uploadProfilePicInput");
+
+uploadPicInput.addEventListener("change", () => {
+    UploadProfilePic();
+});
 
 window.addEventListener("load", () => {
-    ShowLoggedInUserInfo();
+    DownloadProfilePic();
 });
+
+async function UploadProfilePic(){
+    const filesList = uploadPicInput.files;
+    const uploadPromise = await uploadBytes(storageRef, filesList[0]).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
+      DownloadProfilePic();
+}
+
+async function DownloadProfilePic(){
+    getDownloadURL(ref(storage, 'profilePic'))
+  .then((url) => {
+    // Or inserted into an <img> element
+    const img = document.getElementById('profilePic');
+    console.log(url);
+    sessionStorage.setItem("profilePic", url);
+    ShowLoggedInUserInfo();
+  })
+  .catch((error) => {
+    // Handle any errors
+  });
+}
--- public/firebaseinit.js
+++ public/firebaseinit.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -6,6 +8,7 @@ const firebaseConfig = {
   // ...
   // The value of `databaseURL` depends on the location of the database
   databaseURL: "https://swe-4713-applications-domain-default-rtdb.firebaseio.com/",
+  storageBucket: 'gs://swe-4713-applications-domain.appspot.com'
 };
 
 // Initialize Firebase
