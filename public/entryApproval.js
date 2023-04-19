e3058f4315d5e57dd95f66c93a14709b780d798a
ahinson5 <55867273+ahinson5@users.noreply.github.com>
2023-04-19 12:12:10 -0400

    EntryApproval/index/forgotPassword

--- public/entryApproval.js
+++ public/entryApproval.js
@@ -1,6 +1,7 @@
 import { app } from "./firebaseinit";
-import { child, get, getDatabase, ref, set } from "firebase/database";
+import { child, get, getDatabase, ref, set, update } from "firebase/database";
 import { ShowLoggedInUserInfo } from "./MyUtil";
+//import { event } from "../node_modules/firebase-functions/lib/v1/providers/analytics";
 
 
 const entryTable = document.getElementById("entryDisplay");
@@ -8,18 +9,23 @@ const rows = entryTable.getElementsByTagName("tr");
 const getEntryBtn = document.getElementById("getEntryBtn");
 const approveBtn = document.getElementById("approveBtn");
 const denyBtn = document.getElementById("denyBtn");
-const backBtn = document.getElementById("backBtn");
-var arrayIt = 0;
-var displayIt = 0;
+var arrayCheck = 0;
 const entryArray = [];
+const nameArray = [];
 const db = getDatabase(app);
 const dbRef = ref(db)
+const table = document.getElementById("entryDisplay");
 
 window.addEventListener('load', (event) => {
     console.log("window load")
     ShowLoggedInUserInfo();
     getEntries();
     //displayToTable();
+});
+
+getEntryBtn.addEventListener("click", (event) => {
+    console.log("getNewEntryButton");
+    displayToTable();
 });
 
 approveBtn.addEventListener("click", (event) => {
@@ -33,50 +39,57 @@ denyBtn.addEventListener("click", (event) => {
 
 function displayToTable() {
     console.log("displayToTable func entered");
-    const table = document.getElementById("entryDisplay");
     let size = entryArray.length;
     const loadArray = [];
     if (entryArray.length != 0) {//if array of children is not empty
         //date, item, post ref.,debit ,credit, account no
         //child blocks
         //parse blocks into substrings and store each individual string in new row of column
-        loadArray[0] = ParseCSV(entryArray[size - 1].Date.value); 
-        loadArray[1] = ParseCSV(entryArray[size - 1].Item.value);
-        loadArray[2] = ParseCSV(entryArray[size - 1].val().PostRef);
-        loadArray[3] = ParseCSV(entryArray[size - 1].val().Debit);
-        loadArray[4] = ParseCSV(entryArray[size - 1].val().Credit);
-        for (var x = 0; x < 3; x++) {//build string components
-            var Date = ' ';
-            var Item = ' ';
-            var PostRef = ' ';
-            var Debit = ' ';
-            var Credit = ' ';
-            if (loadArray[x][0] !== undefined) { Date = loadArray[x][0]; };
-            if (loadArray[x][1] !== undefined) { Item = loadArray[x][1]; };
-            if (loadArray[x][2] !== undefined) { PostRef = loadArray[x][2]; };
-            if (loadArray[x][3] !== undefined) { Debit = loadArray[x][3]; };
-            if (loadArray[x][4] !== undefined) { Credit = loadArray[x][4]; };
-            //build row from strings and append to table
-            table += '<tr><td>' + Date + '</td><td>' + Item + '</td><td>' + PostRef + '</td><td>' + Debit + '</td><td>' + Credit + '</td></tr>';
+        var Date = ' ';
+        var Accounts = ' ';
+        var PostRef = ' ';
+        var Debits = ' ';
+        var Credits = ' ';
+        loadArray[0] = ParseCSV(entryArray[size - 1].Date); 
+        loadArray[1] = ParseCSV(entryArray[size - 1].Accounts);
+        loadArray[2] = ParseCSV(entryArray[size - 1].PostRef);
+        loadArray[3] = ParseCSV(entryArray[size - 1].Debits);
+        loadArray[4] = ParseCSV(entryArray[size - 1].Credits);
+        //build row from strings and append to table
+        var row = table.insertRow(1);
+        var cell;
+        for (var x = 0; x < 5; x++) {
+            cell = row.insertCell(x);
+            cell.innerHTML = loadArray[x];
+            console.log(loadArray[x]);
         }
     } else {
         console.log("no more entries to evaluate")
     }
 };
 async function getEntries() {
-    console.log("getEntries function entered");
+    //console.log("getEntries function entered");
     await get(child(dbRef, `Journal/`)).then((snapshot) => {
-        console.log("GNE");
+        //console.log("GNE");
         if (snapshot.exists()) {
-            console.log("Journal snapshot exists");
+            //console.log("Journal snapshot exists");
             snapshot.forEach((child) => {//gets the account child
-                var localChild = child;
-                console.log(localChild.val());
+                const localChild = child;
+                const childName = localChild.key;
+                console.log(childName);
+                console.log(localChild.key);
                 if (localChild.child("Approved").val() == "Pending") {//checks acount childs "Approved" child for "pending"
-                    entryArray.push(localChild.val());//stores account childs name in array if its value is "pending"
-                    console.log(entryArray[(entryArray.length) - 1]);
+                    entryArray.push(localChild.val());//stores account childs values in array if its value is "pending"
+                    nameArray.push(childName);//stores account childs name in array if its value is "pending"
+                    arrayCheck = 1;
                 }
             })
+        }
+        if (arrayCheck == 0) {
+            var row = table.insertRow(1);
+            var cell = row.insertCell(0);
+            cell.innerHTML = "No pending entries";
+            console.log("No pending entries");
         }
         console.log(entryArray.length);
     }).catch((error) => {
@@ -85,28 +98,42 @@ async function getEntries() {
 };
 
 async function setApprove() {
-    if (entryArray[arrayIt] != null) {
-        await set(ref(db, 'Journal' + entryArray[arrayIt]), {
-            Approved: 'Approved'
-        });
-        arrayIt++;
-    } else {
-        console.log("End of array")
-    }
+    await get(child(ref(db), `Journal/` + nameArray[nameArray.length - 1])).then((snapshot) => {
+        console.log("snapshot");
+        if (snapshot.exists()) {
+            update(ref(db, 'Journal/' + nameArray[nameArray.length - 1]), {
+                Approved: 'Approved'
+            });
+        } else {
+            console.log("No pending entries")
+        }
+        table.deleteRow(1);
+    }).catch((error) => {
+        console.error(error);
+    });
 };
 
 async function setDenied() {
-    if (entryArray[arrayIt] != null) {
-        await set(ref(db, 'Journal' + entryArray[arrayIt]), {
-            Approved: 'Denied'
-        });
-        arrayIt++;
-    } else {
-        console.log("End of array")
-    }
+    await get(child(ref(db), `Journal/` + nameArray[nameArray.length - 1])).then((snapshot) => {
+        console.log("snapshot");
+        if (snapshot.exists()) {
+            update(ref(db, 'Journal/' + nameArray[nameArray.length - 1]), {
+                Approved: 'Approved'
+            });
+        } else {
+            var row = table.insertRow(1);
+            cell = row.insertCell(1);
+            cell.innerHTML = "No pending entries";
+            //console.log("No pending entries");
+        }
+        table.deleteRow(1);
+    }).catch((error) => {
+        console.error(error);
+    });
 };
 
 function ParseCSV(inputString) {
+    console.log("in csv"+inputString);
     const stringArr = inputString.split(",");
     var formattedString = "";
     for (var i = 0; i < stringArr.length; i++) {
@@ -117,7 +144,7 @@ function ParseCSV(inputString) {
         formattedString += stringArr[i] + newLine;
     }
     return formattedString;
-}
+    };
 
 /* variable setup and html references
 var form = document.querySelector("#createNewUserForm");
--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 import { app } from "./firebaseinit";
-import { get, child, getDatabase, ref, set } from "firebase/database";
+import { get, child, getDatabase, ref, set, update } from "firebase/database";
 //import { user } from "../node_modules/firebase-functions/lib/v1/providers/auth";
 
 //npx kill-port 9099 5001 8075 9000 5000 8085
@@ -12,19 +12,30 @@ var form = document.querySelector("#loginHandlingForm");
 var infoLabel = document.querySelector("#loginInfoLabel");
 var usernameBox = form.querySelector("input[id ='username']");
 var passwordBox = form.querySelector("input[id ='password']");
+const db = getDatabase(app);
+const dbRef = ref(getDatabase(app));
 //Listener
 form.addEventListener("submit", loginProc);
 
 //login function checks failed attempts on username
 //Im 
 function loginProc() {
-    const dbRef = ref(getDatabase(app));
     console.log(i);
-    if (i == 3) {//to many attempts catch
+    if (i == 4) {//to many attempts catch
         loginInfoLabel.textContent = 'Too many failed attempts, refresh page.';
-        i = 0;
+        get(child(dbRef, `users/` + usernameBox.value)).then((snapshot) => {
+            if (snapshot.exists()) {
+                update(ref(db, 'Journal/' + nameArray[nameArray.length - 1]), {
+                    Password: ''
+                });
+                console.log("password lockout");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
         return;
     } else {
+        i += 1;
         get(child(dbRef, `users/`+ usernameBox.value)).then((snapshot) => {
             if (snapshot.exists()) {
                 if (snapshot.val().userPW == passwordBox.value) {
